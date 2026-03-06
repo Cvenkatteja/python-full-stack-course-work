aa711c50c7df29840dcbace21cac1e8b3b7f732d
C.venkatteja <tejav7236@gmail.com>
2026-03-06 23:27:27 +0530

    Add files via upload

---
+++ DAY 1/day 2/student_info.py
@@ -0,0 +1,29 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Student_ID = 'CGH2884 Batch_No='PFS-HYD-050'
+... Email_ID= 'tejav7236@gmail.com'
+... Age=23
+... State='Anantapur'
+... Location='hyderabad'
+... Student_No='+916302568815'
+... Parent_No='+918897431688'
+... Github='https://github.com/Cvenkatteja'
+... College_Name='Gnanamani college of technology'
+... USN_Number=620822106008
+... Department='B.Tech - Electronics and Communication Engineering (ECE)'
+... Qualification='UG (Bachelor Degrees)'
+... Graduation ='76%'
+... Year_of_Passing= 2026
+... Skills=('Python', 'Flask', 'HTML', 'CSS')
+... Mode_of_Study='Offline'
+... 
+... print('------------Personal Information----------------')
+... print(f'Student ID: {Student_ID}')
+... print(f'Batch No: {Batch_No}')
+... print(f'Email ID: {Email_ID}')
+... 
+... 
+... print('------------Academic Information----------------')
+... print(f'Department: {Department}')
+... print(f'College Name: {College_Name}')
+... print(f'Qualification: {Qualification}')
