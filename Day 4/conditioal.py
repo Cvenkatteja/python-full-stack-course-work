ed4a5f90701f18ac454e9d16061b8cd6807fa636
C.venkatteja <tejav7236@gmail.com>
2026-03-10 23:22:46 +0530

    Add files via upload

---
+++ Day 4/conditioal.py
@@ -0,0 +1,83 @@
+
+#simple if condition
+"""if cond:"""
+'if cond:'
+# if else condition
+"""if cond:
+#action
+else:
+#action"""
+'if cond:\n#action\nelse:\n#action'
+#elif condtion statement
+"""if first
+elif second
+else third"""
+'if first\nelif second\nelse third'
+#elif can tkeinfinte  time
+'''min_balance = 5000
+cur_balance = 8000
+
+if cur_balance < min_balance:
+    print("send the amount in the form message ")
+else:
+    print("cut the amount")
+    '''
+'''
+min_charge = 20
+cur_charge = 150
+
+if min_charge > cur_charge:
+    print("send the notification")
+else:
+    print("not the notification")'''
+
+'''data = ('tejav@gmail.com','username@123')
+mail=input("enter the mail: ")
+password=input("enter the password: ")
+
+if data == (mail,password):
+    print("login successfully")
+else:
+    print("invald login")'''
+
+'''fruits = ['mango','papaya','pineapple','ornage']
+search_items = input("search the fruits: ")
+
+if search_items in fruits:
+    print(f"{search_items} there is fruits!")
+else:
+    print(f"{search_items}there is no fruits out of stock"'''
+
+'''time = int(input("enter the hour :"))
+print("avalible buses")
+
+if 6 <= time <= 10:
+    print("bus1\nbus3\nbus4\nbus7")
+elif 10 <= time <=14:
+    print("bus5\nbus6\nbus9\nbus8")
+elif 12 <= time <=20:
+    print("there is no bus")
+else:
+    print("the bus timingis over")'''
+
+'''print("welcom to the uber,book the ride")
+print("1.bike")
+print("2.car")
+print("3.cab")
+
+choice =int(input("enter ur ride: "))
+if choice==1:
+    print("i booked the bike succeessfully\nits on the way")
+elif choice ==2:
+    print("i booked the car successfully\nits on the way")
+elif choice==3:
+    print("ibooked the cab successfully\n its on the way")'''     
+    
+    
+    
+    
+
+
+
+
+    
